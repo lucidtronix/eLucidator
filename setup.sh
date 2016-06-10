b820d4ff5fc09e41defa28b37957a9d696145946
Samwell Freeman <lucidtronix@gmail.com>
2016-06-10 15:37:42 -0700

    added shutdown

--- setup.sh
+++ setup.sh
@@ -3,4 +3,5 @@ cp config_touchscreen.txt /boot/config.txt \
 && mkdir /etc/X11/xorg.conf.d \
 && cp 99-calibration /etc/X11/xorg.conf.d/ \
 && pip install python-instagram \
-&& pip install pi3d
+&& pip install pi3d \
+&& shutdown -r now
