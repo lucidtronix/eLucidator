d0d43ede75fd1e83712e03491e594ecada71427f
Samwell Freeman <lucidtronix@gmail.com>
2016-07-06 10:09:46 -0700

    audio to headphones by default

--- setup.sh
+++ setup.sh
@@ -21,6 +21,7 @@ sudo apt-get update \
 && sudo make install \
 && sudo ldconfig \
 && cp config_touchscreen.txt /boot/config.txt \
+&& amixer cset numid=3 1 # set audio to headphone jack
 && mkdir /etc/X11/xorg.conf.d \
 && cp 99-calibration /etc/X11/xorg.conf.d/ \
 && pip install --upgrade python-instagram \
