baa24980ab99d42ee890d424b17f7740515ef11b
Samwell Freeman <lucidtronix@gmail.com>
2016-09-06 16:00:39 -0400

    slideshow integrated with imageRow

---
+++ lucidapp/ImageRow.py
@@ -0,0 +1,106 @@
+# ImageRow.py
+# ImageRow for LucidTronix eLucidator Applications
+# A row of images loaded from an ImageStream that support basic touch screen interactions
+# Samwell Freeman
+# June 2016
+
+import os
+import sys
+import cv2
+import pygame
+import random
+import argparse
+import threading
+import numpy as np
+from time import time
+from PIL import Image
+import urllib, cStringIO
+from TouchScreen import TouchScreen
+from LucidApp import LucidApp, Button
+from ImageStreamDir import ImageStreamDir
+
+class ImageRow:
+	def __init__(self, parent, stream, ts, resolution, wrap=True):
+		self.parent = parent
+		self.stream = stream
+		self.ts = ts
+		self.resolution = resolution
+		self.wrap = wrap
+		self.margin = 10
+		self.corner = (self.margin, 7*self.margin)
+		self.last_corner = (self.margin, 7*self.margin)
+		self.images = []
+		self.redraw = False
+		self.cur_image = 1
+		self.was_easing = False
+
+
+	def update(self):
+		if len(self.images) > 2:
+			
+			if self.ts.sliding or self.ts.easing:
+				if self.was_easing and self.ts.sliding:
+					self.last_corner = self.corner
+				self.corner = (self.last_corner[0] + self.ts.delta[0], self.corner[1])
+				self.redraw = True
+				if self.corner[0] + self.image_widths() < self.resolution[0]:
+					self.cur_image = (self.cur_image+1)%len(self.images)
+					self.corner = (self.margin, self.corner[1])
+					self.last_corner = (self.last_corner[0] + self.images[self.prev_index()].get_size()[0], self.corner[1])
+				elif self.corner[0] > self.images[self.cur_image].get_size()[0]+self.margin:
+					self.corner = (self.corner[0]-self.images[self.cur_image].get_size()[0], self.corner[1])
+					self.last_corner = (self.last_corner[0] -self.images[self.cur_image].get_size()[0], self.corner[1])
+					self.cur_image = (self.cur_image-1)%len(self.images)
+			else:
+				self.last_corner = self.corner
+				self.redraw = False
+
+			if self.ts.easing:
+				self.was_easing = True
+			else:
+				self.was_easing = False
+
+			if self.images[self.next_index()].error:
+				del self.images[self.next_index()]
+
+		if self.stream.size() > len(self.images):
+			self.images.append(self.stream.next())
+			print ' Adding image from stream...',  self.images[len(self.images)-1].img_path
+
+
+	def image_widths(self):
+		if self.corner[0] < 0:
+			return self.images[self.cur_image].get_size()[0] + self.images[self.next_index()].get_size()[0] + (2*self.margin)
+		else:
+			return self.images[self.cur_image].get_size()[0] + self.images[self.prev_index()].get_size()[0] + (2*self.margin)
+
+
+	def prev_index(self):
+		return (self.cur_image-1) % len(self.images)
+
+	def prev_prev_index(self):
+		return (self.cur_image-2) % len(self.images)
+
+	def next_index(self):
+		return (self.cur_image+1) % len(self.images)		
+
+	def next_next_index(self):
+		return (self.cur_image+2) % len(self.images)
+
+	def display(self):
+		if len(self.images) > 2:
+			self.parent.fill()
+			self.parent.show_image(self.images[self.cur_image], self.corner)
+			c_img_x = self.images[self.cur_image].get_size()[0]
+			rp = (self.corner[0] + c_img_x +self.margin, self.corner[1])
+			lp = (self.corner[0] - (self.images[self.prev_index()].get_size()[0]+self.margin), self.corner[1])
+			if self.corner[0] < 0:
+				self.parent.show_image(self.images[self.next_index()], rp)
+				if self.corner[0] > self.margin:
+					self.parent.show_image(self.images[self.prev_index()], lp)
+			else:
+				self.parent.show_image(self.images[self.prev_index()], lp)	
+				if rp[0] < self.resolution[0]:
+					self.parent.show_image(self.images[self.next_index()], rp)
+
+
---
+++ lucidapp/SlideshowTouch.py
@@ -0,0 +1,120 @@
+# Slideshow.py
+# Slideshow Class for LucidTronix eLucidator Applications
+# Samwell Freeman
+# June 2016
+
+import os
+import sys
+import cv2
+import pygame
+import threading
+import numpy as np
+from time import time
+from PIL import Image
+import urllib, cStringIO
+from LucidApp import LucidApp, Button
+from ImageRow import ImageRow
+from TouchScreen import TouchScreen
+from ImageStream import InternetImage
+from ImageStreamDir import ImageStreamDir
+from ImageStreamGoogle import ImageStreamGoogle
+
+class SlideshowTouch(LucidApp):
+	def __init__(self, ts=None, cache_path='./cache/', fullscreen=False, resolution=(800, 400), 
+					icon_path='./icons/slideshow.png', base_graphics='cv2'):
+		super(SlideshowTouch, self).__init__('SlideshowTouch', cache_path, fullscreen, resolution, icon_path, base_graphics)
+		self.playing = True
+		self.stream = ImageStreamDir(load_strategy='all')
+
+		if ts:
+			self.ts = ts
+		else:
+			self.ts = TouchScreen()
+
+		self.delay = 5.0 # seconds
+		self.last_update = time() - self.delay
+		self.fading = False
+		self.fade_time = 2.0 # seconds
+		self.fade_start = time()
+		self.last_image = None
+		self.cur_image = None
+		self.image_corner = (30,90)
+
+		self.buttons.append(Button(self, 'next', (260,10,80,40), (50,50,50), self.next))
+		self.buttons.append(Button(self, 'previous', (100,10,145,40), (50,50,50), self.prev_image))
+		self.play_pause_btn = Button(self, 'pause', (360,10,100,40), (50,50,50), self.play_pause)
+		self.buttons.append(self.play_pause_btn)
+
+		self.row = ImageRow(self, self.stream, self.ts, self.resolution)
+
+	def next_image(self):
+		self.last_image = self.cur_image
+		if self.last_image is not None:
+			self.fading = True
+			self.fade_start = time()
+		self.cur_image = self.stream.next()
+		self.last_update = time()		
+
+	def prev_image(self):
+		self.last_image = self.stream.prev()
+		self.cur_image = self.last_image
+		self.last_update = time()
+		return 0	
+
+	def play_pause(self):
+		self.playing = not self.playing
+		if self.playing:
+			self.play_pause_btn.name = 'pause'
+			self.play_pause_btn.rect = (360,10,100,40)
+		else:
+			self.play_pause_btn.name = 'play'
+			self.play_pause_btn.rect = (360,10,72,40)
+		return 0
+
+
+	def run(self):
+		cx = oldx = 0
+		pic_update = 0
+
+		while True:
+			self.ts.update()
+			self.row.update()
+			self.row.display()
+			
+			ret = self.handle_keys()
+			if ret <= 0:
+				return ret
+
+			for b in self.buttons:
+				if b.over(self.ts.mx, self.ts.my) and self.ts.double_tap and time()-b.last_press > 0.5:
+					ret = b.press() 
+					if ret < 0:
+						return ret
+					self.ts.double_tap = False
+
+					self.row.images[self.row.cur_image] = self.cur_image
+				b.show()
+
+			if self.playing:
+				if time()-self.last_update > self.delay:
+					self.next_image()
+
+				if self.fading:
+					alpha =	(time()-self.fade_start) / self.fade_time 
+					if alpha > 1.0:
+						self.fading = False
+
+					beta = (1.0 - alpha)
+					blend = cv2.addWeighted(self.last_image.to_array(), beta, self.cur_image.to_array(), alpha, 0.0)
+					self.row.images[self.row.cur_image] = InternetImage(cv_img=blend)
+					
+			self.draw()
+
+	def next(self):
+		self.next_image()
+		self.fading = False
+		return 0
+
+if __name__ == '__main__':
+	app = SlideshowTouch()
+	app.run()
