ae378b1ee78087d82a4cfc1631ce5ddb7a276b64
Samwell Freeman <lucidtronix@gmail.com>
2016-10-02 22:23:37 -0400

    mandelbrot tiling

--- lucidapp/Mandelbrot.py
+++ lucidapp/Mandelbrot.py
@@ -27,16 +27,20 @@ class Mandelbrot(LucidApp):
 		self.ts = ts if ts else TouchScreen()
 		self.last_update = time() 
 		#self.buttons.append(Button(self, 'next', (260,10,80,40), (50,50,50), self.next))
-		#self.buttons.append(Button(self, 'previous', (100,10,145,40), (50,50,50), self.prev_image))
 		self.cur_color = (128,128,128)
 		self.bg_color = (0,0,0)
-		self.mandelbrot_image = np.zeros((self.resolution[1] - 20, self.resolution[0] -20, 3))
+
+		# x1, x2, y1, y2
+		self.mandelbrot_shape = (20, 450, 90, 310)
 		self.x_range = (-0.74888, -0.74877)   # (-2.0, 0.5) # 
 		self.y_range =  (0.06515, 0.06525)# (-1.5, 1.5) # 
+		#self.x_range = (-2.0, 0.5) # 
+		#self.y_range = (-1.5, 1.5) # 
 		self.zoom = 1.0
 		self.maxiter = 1024
 		self.cur_x = 0
 		self.cur_y = 0
+		self.level = 1
 
 	def __str__(self):
 		return 'Mandelbrot'
@@ -44,9 +48,13 @@ class Mandelbrot(LucidApp):
 
 	def run(self):
 		quit = False
+		tiles_at_this_level = 2**self.level
+		xpix = self.mandelbrot_width() / tiles_at_this_level
+		ypix = self.mandelbrot_height() / tiles_at_this_level
+		self.label(self.name, 70, 320, font_scale=1.1)
+
 		while not quit:
 			self.ts.update()
-
 			ret = self.handle_keys()
 			if ret < 0:
 				quit = True
@@ -59,26 +67,58 @@ class Mandelbrot(LucidApp):
 					self.ts.double_tap = False
 				b.show()
 			
-			x_ratio = float(self.cur_x) / self.mandelbrot_image.shape[0]
-			scale_x = self.x_range[0] + x_ratio*(self.x_range[1]-self.x_range[0])
-			y_ratio = float(self.cur_y) / self.mandelbrot_image.shape[1]
-			scale_y = self.y_range[0] + y_ratio*(self.y_range[1]-self.y_range[0])            
 
-			z = scale_x + 1j*scale_y 
-			f_of_z = mandelbrot(z, self.maxiter)
-
-			self.canvas[self.cur_x,self.cur_y,:] = i_to_rgb(f_of_z)
+			x1 = self.cur_x*xpix
+			x2 = (self.cur_x+1)*xpix
+			y1 = self.cur_y*ypix
+			y2 = (self.cur_y+1)*ypix
 			
+			self.color_tile((x1,x2,y1,y2))
+
 			self.cur_x += 1
-			if self.cur_x >= self.mandelbrot_image.shape[0]:
+			if self.cur_x > tiles_at_this_level-1:
 				self.cur_y += 1
 				self.cur_x = 0
-				if self.cur_y >= self.mandelbrot_image.shape[1]:
+				if self.cur_y > tiles_at_this_level-1:
 					self.cur_y = 0
 					self.cur_x = 0
+					self.level += 1
+					tiles_at_this_level = 2**self.level
+					xpix = max(1, self.mandelbrot_width() / tiles_at_this_level)
+					ypix = max(1, self.mandelbrot_height() / tiles_at_this_level)
+
+					print 'going to next level:', self.level, 'tiles here:', tiles_at_this_level, 'xpix:', xpix, 'ypix:', ypix
 			
-			self.label(self.name, 70, 300, font_scale=1.5, alpha = 0.5)
 			self.draw()
+
+	def mandelbrot_width(self):
+		return self.mandelbrot_shape[1]-self.mandelbrot_shape[0]
+
+	def mandelbrot_height(self):
+		return self.mandelbrot_shape[3]-self.mandelbrot_shape[2]
+		
+ 
+	def color_tile(self, tile):
+		sample_x = (tile[0] + tile[1]) / 2
+		sample_y = (tile[2] + tile[3]) / 2
+
+		yb1 = self.mandelbrot_shape[2]+tile[2]
+		yb2 = self.mandelbrot_shape[2]+tile[3]
+		xb1 = self.mandelbrot_shape[0]+tile[0]
+		xb2 = self.mandelbrot_shape[0]+tile[1]
+
+		self.canvas[yb1:yb2, xb1:xb2, :] = self.color_from_point(sample_x, sample_y)
+
+	def color_from_point(self, x, y):
+		x_ratio = float(x) / self.mandelbrot_width()
+		scale_x = self.x_range[0] + x_ratio*(self.x_range[1]-self.x_range[0])
+		y_ratio = float(y) / self.mandelbrot_height()
+		scale_y = self.y_range[0] + y_ratio*(self.y_range[1]-self.y_range[0])            
+
+		z = scale_x + 1j*scale_y 
+		f_of_z = mandelbrot(z, self.maxiter)
+
+		return i_to_rgb(f_of_z)	
 
 
 # resolution = (1024, 1024, 3)
@@ -94,30 +134,6 @@ def mandelbrot(z,maxiter):
           return n
       z = z*z + c
   return maxiter
-
-# def mandelbrot_set(xmin,xmax,ymin,ymax,width,height,maxiter):
-#     r1 = np.linspace(xmin, xmax, width)
-#     r2 = np.linspace(ymin, ymax, height)
-#     n3 = np.empty((width,height, 3))
-#     for i in range(width):
-#         for j in range(height):
-#             m = mandelbrot(r1[i] + 1j*r2[j],maxiter)
-#             n3[i,j,:] = i_to_rgb(m)
-#     return n3
-
-
-# def run_m2(width,height):
-
-#   #img = mandelbrot_set(-2.4,0.5,-1.25,1.25,resolution[0], resolution[1], 1024)
-#   img = mandelbrot_set(-0.74888, -0.74877, 0.06515, 0.06525, width, height, 1200)
-#   print 'got img shape:', img.shape, 'min: ', img.min(), 'max:',  img.max()
-#   img -= img.min()
-#   img *=  255.0/img.max()
-#   img = np.uint8(img)
-#   while True:
-#     cv2.imshow('Image', img)
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
-#       break
 
 if __name__ == '__main__':
 	app = Mandelbrot()
